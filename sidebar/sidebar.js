4dd4a165ca61b5e7f90edb21f74850023a397bac
Jerry Odeghe <odeghejerry@gmail.com>
2019-12-01 08:46:28 +0100

    navbar

---
+++ sidebar/sidebar.js
@@ -0,0 +1,5 @@
+document.getElementById('sidebar_toggle').addEventListener('click', toggleSidebar);
+
+function toggleSidebar() {
+    document.getElementById('sidebar').classList.toggle('active');
+}
